3ce660306455df9a651d202487f4438f155cdd91
Maxi Romero <knmaxi888@gmail.com>
2026-06-23 13:57:44 -0300

    Remove crossorigin from script/link tags for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,19 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/myportafolio1/',
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    {
+      name: 'remove-crossorigin',
+      transformIndexHtml(html) {
+        return html
+          .replace(/<script([^>]*?)crossorigin/g, '<script$1')
+          .replace(/<link([^>]*?)crossorigin([^>]*?)>/g, (match) => {
+            if (match.includes('google')) return match
+            return match.replace(/ crossorigin/g, '')
+          })
+      },
+    },
+  ],
 })
